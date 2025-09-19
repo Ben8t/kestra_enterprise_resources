6353b4652dd7b779324d42e7344999602089a35b
Benoit <benoit@kestra.io>
2025-09-19 14:26:42 +0200

    feat(test): add unit test

---
+++ terraform/tests.tf
@@ -0,0 +1,6 @@
+resource "kestra_test" "test" {
+  depends_on = [kestra_flow.flows]
+  test_id    = "test-extract"
+  content    = file("${path.module}/../resources/tests/test-extract.yaml")
+  namespace  = "kestra.weather"
+}
